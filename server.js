431f3b403e4f7fda56d3c8c7daf063f5ee6b9044
Adel Stiti <adelw126@gmail.com>
2022-05-15 17:47:32 +0100

    Add home route

--- server.js
+++ server.js
@@ -15,9 +15,21 @@ connectDB();
 // Init bodyParser middleware
 app.use(express.json());
 
-
 // Define Routes
 app.use('/api/planes',require('./routes/planes'));
+
+
+// Home Route
+app.get('/',(req,res) => {
+    res.status(200).json(
+        {
+            msg:'Welcome to the Plane API', 
+            version: '1.0.0', 
+            author: 'Adel Stiti', 
+            email: 'adelstiti@gmail.com'
+        }
+    );
+});
 
 const PORT = process.env.PORT || 5000 ;
 
